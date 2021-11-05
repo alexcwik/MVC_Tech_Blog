51b6790abaa30b79f244a9a834fb857d38e8c45a
Alex Cwik <alexcwik1490@gmail.com>
2021-11-05 13:24:08 -0500

    added server file

--- server.js
+++ server.js
@@ -9,6 +9,38 @@ const sequelize = require('./config/connection');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Top secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '[public')));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
+
+
+
 
 
 
